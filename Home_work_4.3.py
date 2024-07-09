584edd19a3bb775485d9451f8dab37213097084c
volodymyr <kybavolodymyr@gmail.com>
2024-07-09 14:53:12 +0200

    Home_work_4

--- Home_work_4.3.py
+++ Home_work_4.3.py
@@ -1,12 +1,27 @@
-import pathlib
 import sys
-a = sys.argv
-path = pathlib.Path("a")
-for root, dirs, files in path.walk(): 
-    print("Root: ") 
-    print(root) 
-    print("Dirs: ") 
-    print(dirs) 
-    print("Files: ") 
-    print(files) 
-    print("")
+from colorama import Fore, Style
+from pathlib import Path
+
+def display_directory_structure(directory_path, indentation=''):
+    directory = Path(directory_path)
+    try:
+        for entry in directory.iterdir():
+            if entry.is_dir():
+                print (f"{Fore.BLUE}{indentation}{entry.name}{Style.RESET_ALL}")
+                display_directory_structure(entry, indentation + '    ')
+            else:
+                print(f"{Fore.GREEN}{indentation}{entry.name}{Style.RESET_ALL}")
+    except FileNotFoundError:
+        print(Fore.RED + "Директорія не існує.")
+        sys.exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        print("Використання: python script.py /шлях/до/директорії")
+        sys.exit(1)
+
+    directory_path = sys.argv[1]
+    display_directory_structure(directory_path)
+
+if __name__ == "__main__":
+    main()
